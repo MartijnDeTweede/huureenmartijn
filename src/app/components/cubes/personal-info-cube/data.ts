d0286c61a58b4331dfe8192aca7f1be1ae60bb7e
Martijn Schotel <schotel.mc@gmail.com>
2020-03-27 18:32:22 +0100

    refactored data to data.json

--- src/app/components/cubes/personal-info-cube/data.ts
+++ src/app/components/cubes/personal-info-cube/data.ts
@@ -29,6 +29,63 @@ const data = {
     "Kookt altijd met verse groente",
     "Favoriete boek: Mistborn",
     "Favoriete film: Coach Carter",
+  ],
+  educations: [
+    {
+      degree:"HBO Commerciële Economie",
+      educator:"Leidsche Onderwijs Instellingen",
+      startDate:"02/2012",
+      endDate:"10/2014",
+    },
+    {
+      degree:"HBO Chemie",
+      educator:"Hogeschool Arnhem en Nijmegen",
+      startDate:"07/2007",
+      endDate:"11/2011",
+    },
+    {
+      degree:"Havo",
+      educator:"Maaswaal College",
+      startDate:"09/2002",
+      endDate:"07/2007",
+    },
+  ],
+  trainings: [
+    {
+      training:"Microsoft Certified: Azure Fundamentals",
+      status:"Huidig",
+      year:"2020",
+    },
+    {
+      training:"Microsoft 70-483",
+      status:"Behaald",
+      year:"2015",
+    },
+    {
+      training:"MScrum PSM 1",
+      status:"Behaald",
+      year:"2015",
+    },
+    {
+      training:"Business English Certificate Vantage",
+      status:"Behaald",
+      year:"2013",
+    },
+    {
+      training:"Nima B consumentenMarketing",
+      status:"Behaald",
+      year:"2013",
+    },
+    {
+      training:"Exin I-track: Project participation foundation",
+      status:"Behaald",
+      year:"2013",
+    },
+    {
+      training:"Nima A",
+      status:"Behaald",
+      year:"2012",
+    },
   ]
 }
 
--- src/app/components/cubes/personal-info-cube/personal-info-cube.component.ts
+++ src/app/components/cubes/personal-info-cube/personal-info-cube.component.ts
@@ -12,7 +12,9 @@ export class PersonalInfoCubeComponent implements OnInit {
   public personalia = data.personalia;
   public interests = data.interests;
   public facts = data.facts;
-
+  public educations = data.educations;
+  public trainings = data.trainings;
+  
   constructor() { }
 
   ngOnInit(): void {
